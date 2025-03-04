5cb126572b73b92ed843381174d845d575ed7b62
Le Thanh Nguyen <u7594144@anu.edu.au>
2025-03-05 01:37:45 +1100

    init project

---
+++ src/test/java/com/example/nba/NbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
